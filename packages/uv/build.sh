97b0fe5cce468d8ac8bded176fef90293c135df9
Karan <kdhingra307@gmail.com>
2025-05-02 05:10:22 +0530

    f

--- packages/uv/build.sh
+++ packages/uv/build.sh
@@ -33,9 +33,9 @@ termux_step_pre_configure() {
 }
 
 termux_step_make() {
-	PKG_CONFIG_ALL_DYNAMIC=1 \
-	ZSTD_SYS_USE_PKG_CONFIG=1 \
-	cargo build --jobs "${TERMUX_PKG_MAKE_PROCESSES}" --target "${CARGO_TARGET_NAME}" --release
+# 	PKG_CONFIG_ALL_DYNAMIC=1 \
+# 	ZSTD_SYS_USE_PKG_CONFIG=1 \
+# 	# cargo build --jobs "${TERMUX_PKG_MAKE_PROCESSES}" --target "${CARGO_TARGET_NAME}" --release
 }
 
 termux_step_make_install() {
